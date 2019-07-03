20930fbb2c42aaf7f59b02385036c19f7a59c4af
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2019-07-03 14:14:33 -0300

    Create 1062.c

---
+++ 1062.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    float x;
+    scanf ("%f", &x);
+    if (x > 7.0)
+    {
+        printf ("Basica");
+    }
+    else if (x < 7.0)
+    {
+        printf ("Acida");
+    }
+    else if (x = 7.0)
+    {
+        printf ("Neutra");
+    }
+    return 0;
+}
