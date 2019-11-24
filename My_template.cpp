1e29a21b1d3b14e1b845d980dfa4186181316f1c
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2019-11-23 22:38:16 -0300

    Create My_template.cpp

---
+++ My_template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define lli long long int 
+#define pb push_back 
+#define in (int i = 0 ; i < n ; i++)
+#define jm (int j = 0 ; j < m ; j++)
+
+int main ()
+{
+
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+	
+    return 0 ;
+}
