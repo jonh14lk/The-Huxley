9d7fc65fb161da766ab14958b3449253e84a05a5
João Victor de Alarcão Ayalla Alcântara <50853845+jonh14lk@users.noreply.github.com>
2019-12-14 14:03:22 -0300

    Delete Matrizes.c

--- Matrizes.c
+++
@@ -1,4 +0,0 @@
-# Matrizes na linguagem C
-
-Definição: Matriz é a uma estrutura de dados do tipo vetor com duas ou mais dimensões, cujo os itens de uma matriz tem que ser todos do mesmo tipo de dado.
-Na prática, as matrizes formam tabelas na memória.
